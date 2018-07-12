f000a9d08fa200f67cbea00aa4c2ea38b9f75774
Zhisen J. Urgolites <zhisen.urgolites@gmail.com>
2018-07-11 18:46:47 -0700

    PyParagraph made

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -1,17 +1,54 @@
-import re
 path1 = "/Users/Zhisen/Downloads/paragraph_files/paragraph_1.txt"
 path2 = "/Users/Zhisen/Downloads/paragraph_files/paragraph_2.txt"
 outpath = "/Users/Zhisen/python-challenge/PyParagraph/paragraphs_output.txt"
-with open(path1, 'r') as infile1, \
-     open(path2, 'r') as infile2, \
+with open(path1, 'r',encoding = "utf-8") as infile1, \
+     open(path2, 'r',encoding = "utf-8") as infile2, \
      open(outpath, 'w') as outfile:
-     p1 = infile1.readlines()
-     p2 = infile2
+     p1 = infile1.read()
+     p2 = infile2.read()
      output = outfile
-     word_list = []
-     for word in p1:
-         word_list += word
-     print(len(word_list))
-      
-     p1_list = re.split("(?<=[.!?]) +", p1) 
-     #print(p1_list)
+     words = p1.split()
+     word_count = len(words)
+     sentences = p1.split('.')
+     sentence_count = len(sentences)
+     letter_list = []
+     for word in words:
+         for letter in word:
+              letter_list.append(letter)
+     letter_count = len(letter_list)
+     print('Paragraph Analysis (paragraph_1)')
+     output.write('Paragraph Analysis (paragraph_1)\n')
+     print('------------------------------------------')
+     output.write('------------------------------------------\n')
+     print(f'Approximate Word Count: {word_count}')
+     output.write(f'Approximate Word Count: {word_count}\n')
+     print(f'Approximate Sentence Count: {sentence_count}')
+     output.write(f'Approximate Sentence Count: {sentence_count}\n')
+     print(f'Average Letter Count (per word): {letter_count/word_count: .11f}')
+     output.write(f'Average Letter Count (per word): {letter_count/word_count: .11f}\n')
+     print(f'Average Sentence Length (in words): {word_count/sentence_count: .1f}')
+     output.write(f'Average Sentence Length (in words): {word_count/sentence_count: .1f}\n')
+         
+     words = p2.split()
+     word_count = len(words)
+     sentences = p2.split('.')
+     sentence_count = len(sentences)
+     letter_list = []
+     for word in words:
+         for letter in word:
+             letter_list.append(letter)
+     letter_count = len(letter_list)
+     print('\n')
+     output.write('\n')
+     print('Paragraph Analysis (paragraph_2)')
+     output.write('Paragraph Analysis (paragraph_2)\n')
+     print('------------------------------------------')
+     output.write('------------------------------------------\n')
+     print(f'Approximate Word Count: {word_count}')
+     output.write(f'Approximate Word Count: {word_count}\n')
+     print(f'Approximate Sentence Count: {sentence_count}')
+     output.write(f'Approximate Sentence Count: {sentence_count}\n')
+     print(f'Average Letter Count (per word): {letter_count/word_count: .11f}')
+     output.write(f'Average Letter Count (per word): {letter_count/word_count: .11f}\n')
+     print(f'Average Sentence Length (in words): {word_count/sentence_count: .1f}')
+     output.write(f'Average Sentence Length (in words): {word_count/sentence_count: .1f}\n')
